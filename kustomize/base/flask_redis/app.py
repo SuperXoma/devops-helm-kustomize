771db40ce066306abd339bfcad595674bc1e8eba
SuperXoma <artyom_nad@mail.ru>
2026-03-30 12:59:36 +0000

    Initial project upload

---
+++ kustomize/base/flask_redis/app.py
@@ -0,0 +1,26 @@
+import redis
+from flask import Flask, make_response
+import socket
+import os
+
+DB_HOST = os.getenv('REDIS_HOST', 'redis')
+MY_ENV = os.getenv('ENV', 'unknown')
+
+app = Flask(__name__)
+cache = redis.Redis(host=DB_HOST, port=6379)
+
+def get_hit_count():
+	retries = 5
+	while True:
+		try:
+			return cache.incr('hits')
+		except redis.exceptions.ConnectionError as exc:
+			if retries ==0:
+				raise exc
+			retries -=1
+			time.sleep(0.5)
+
+@app.route('/')
+def hello():
+	count = get_hit_count()
+	return 'Hello World! I have been seen {} times. My name is: {} My env: {}\n'.format(count, socket.gethostname(), MY_ENV)
